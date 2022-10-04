c0ea261aec50597f8bee1ab968eb8511e7aa8e85
Justin W <justinw@archlinux.localdomain>
2022-10-04 21:54:56 +0530

    Modify datasource, dev exts and res

--- lib/str/str_res.dart
+++ lib/str/str_res.dart
@@ -4,9 +4,6 @@
 var aboutUs =
     "Salevy is a fictional brand. No part of this website has been copied from another source. This website is made for the Technophilia 'WebNotch' competition";
 
-var openSource =
-    "This website is completely open source. The source is available at github.";
-
 var authors =
     "This website has been created by Swastik Vats (@idoalotofthings) and Harsh Goyenka (@enjoyinbreaks)";
 
