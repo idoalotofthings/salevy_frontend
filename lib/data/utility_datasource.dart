62f7d1edeb8748fcca9ce83205d5589185eae034
Justin W <justinw@archlinux.localdomain>
2022-10-04 21:54:17 +0530

    Create utility datasource

---
+++ lib/data/utility_datasource.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:salevy_frontend/model/utility.dart';
+
+const utilities = [
+  Utility(
+      description:
+          "Shoes which give you comfort. Salevy exclusively adds a foam layer inside the shoe, giving your legs comfort.",
+      image: Icon(
+        Icons.roller_skating,
+        size: 200,
+      )),
+  Utility(
+      description:
+          "Variety. Who doesn't love it? At Salevy, we make sure that our customers get the most variety at any of our outlets.",
+      image: Icon(
+        Icons.auto_mode,
+        size: 200,
+      )),
+  Utility(
+      description:
+          "And most importantly, a price at which you can agree. We make sure that our customers get our products at the lowest possible prices, without sacrificing the quality of our products.",
+      image: Icon(
+        Icons.attach_money,
+        size: 200,
+      ))
+];
