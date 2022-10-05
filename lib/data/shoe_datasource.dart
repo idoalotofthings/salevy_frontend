e9dc23ef28d45f5087c4b1b1e7a87402589e4e08
Enjoyinbreaks <harshgoyenka@yahoo.com>
2022-10-05 16:03:12 +0530

    Added some shoes

--- lib/data/shoe_datasource.dart
+++ lib/data/shoe_datasource.dart
@@ -1,4 +1,4 @@
-// Datasource file for the shoes. 
+// Datasource file for the shoes.
 
 import 'package:flutter/widgets.dart';
 import 'package:salevy_frontend/model/shoe.dart';
@@ -9,46 +9,54 @@ final shoes = [
       price: 150,
       description: "Made with the best quality leather from the United States",
       origin: "USA",
-      image: const AssetImage("assets/images/sporty.png")
-  ),
-
+      image: const AssetImage("assets/images/sporty.png")),
   Shoe(
       name: "Ocean's Wave",
       price: 200,
       description: "Wear it and fly like the waves of the ocean",
       origin: "UK",
-      image: const AssetImage("assets/images/ocean.png")
-  ),
-
+      image: const AssetImage("assets/images/ocean.png")),
   Shoe(
       name: "Nightly Wear",
       price: 450,
       description: "Make your shoes glow in the dark",
       origin: "Australia",
-      image: const AssetImage("assets/images/nightly.png")
-  ),
-
+      image: const AssetImage("assets/images/nightly.png")),
   Shoe(
       name: "Footballer's Companion",
       price: 500,
       description: "The best soccer shoes you can ever get!",
       origin: "UK",
-      image: const AssetImage("assets/images/football.png")
-  ),
-
+      image: const AssetImage("assets/images/football.png")),
   Shoe(
       name: "Mountaineer's Companion",
       price: 500,
       description: "Give your legs some comfort while you scale the heights",
       origin: "Canada",
-      image: const AssetImage("assets/images/mountain.png")
-  ),
-
+      image: const AssetImage("assets/images/mountain.png")),
   Shoe(
       name: "On The Go",
       price: 300,
       description: "You can wear em' anytime, anyday",
       origin: "India",
-      image: const AssetImage("assets/images/onthego.png")
-  ),
+      image: const AssetImage("assets/images/onthego.png")),
+  Shoe(
+      name: "Loafers",
+      price: 350,
+      description: "Happy Feets! Happy Smiles!",
+      origin: 'Germany',
+      image: const AssetImage("assets/images/loafers.png")),
+  Shoe(
+      name: "Pumps",
+      price: 550,
+      description: "Look party ready al' the time",
+      origin: 'Italy',
+      image: const AssetImage('assets/images/pumps.png')),
+  Shoe(
+      name: 'Sliders',
+      price: 150,
+      description:
+          "It's easire to put on slippers, than to cover whole world with carpet.",
+      origin: 'India',
+      image: AssetImage('assets/images/sliders.png'))
 ];
