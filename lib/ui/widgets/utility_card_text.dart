0f6e2a428217c05ecb74cc2d4f856f6ddce81d9e
Justin W <justinw@archlinux.localdomain>
2022-10-05 13:03:25 +0530

    Fix utility cards fade in animation

--- lib/ui/widgets/utility_card_text.dart
+++ lib/ui/widgets/utility_card_text.dart
@@ -18,62 +18,68 @@ class UtilityCardText extends StatefulWidget {
 class _UtilityCardTextState extends State<UtilityCardText> {
   bool _animate = false;
   static bool _isStarted = true;
-
-  @override
-  void initState() {
-    super.initState();
-    if (_isStarted) {
-      Future.delayed(Duration(milliseconds: widget.index * 100), () {
-        setState(() {
-          _animate = true;
-          _isStarted = false;
-        });
-      });
-    } else {
-      _animate = true;
-    }
-  }
+  bool _start = false;
 
   var opacity = 0;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      opacity: _animate ? 1 : 0,
-      curve: Curves.easeInOutQuart,
-      duration: const Duration(seconds: 1),
-      child: AnimatedPadding(
-        duration: const Duration(seconds: 1),
-        padding: _animate
-            ? const EdgeInsets.all(4.0)
-            : const EdgeInsets.only(top: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                height: 300,
-                width: 400,
-                child: Card(
-                  elevation: 16,
-                  child: widget.utility.image,
+    if (_start) {
+      if (_isStarted) {
+        Future.delayed(Duration(milliseconds: widget.index * 100), () {
+          setState(() {
+            _animate = true;
+            _isStarted = false;
+          });
+        });
+      } else {
+        _animate = true;
+      }
+    }
+
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          _start = true;
+        });
+      },
+      child: AnimatedOpacity(
+        opacity: _animate ? 1 : 0,
+        curve: Curves.easeInOutQuart,
+        duration: const Duration(seconds: 2),
+        child: AnimatedPadding(
+          duration: const Duration(seconds: 2),
+          padding: _animate
+              ? const EdgeInsets.all(4.0)
+              : const EdgeInsets.only(top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: 300,
+                  width: 400,
+                  child: Card(
+                    elevation: 16,
+                    child: widget.utility.image,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 64,
-            ),
-            Container(
-              height: 300,
-              width: 400,
-              alignment: Alignment.center,
-              child: Text(
-                widget.utility.description,
-                style: GoogleFonts.adventPro(fontSize: 32),
+              const SizedBox(
+                width: 64,
               ),
-            )
-          ].reverseIfNeeded(widget.reverse) as List<Widget>,
+              Container(
+                height: 300,
+                width: 400,
+                alignment: Alignment.center,
+                child: Text(
+                  widget.utility.description,
+                  style: GoogleFonts.adventPro(fontSize: 32),
+                ),
+              )
+            ].reverseIfNeeded(widget.reverse) as List<Widget>,
+          ),
         ),
       ),
     );
