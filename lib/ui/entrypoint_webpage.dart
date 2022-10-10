3f7129a801baf75bec91ae20a789cfba2e40bea0
Justin W <justinw@archlinux.localdomain>
2022-10-10 19:30:05 +0530

    Remove app bar from entrypoint webpage

--- lib/ui/entrypoint_webpage.dart
+++ lib/ui/entrypoint_webpage.dart
@@ -3,9 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:salevy_frontend/data/utility_datasource.dart';
 import 'package:salevy_frontend/ui/viewmodel/theme_viewmodel.dart';
 import 'package:salevy_frontend/ui/widgets/about_us_widget.dart';
+import 'package:salevy_frontend/ui/widgets/app_bar.dart';
 import 'package:salevy_frontend/ui/widgets/product_showcase_widget.dart';
 import 'package:salevy_frontend/ui/widgets/utility_card_text.dart';
-import 'package:simple_animated_icon/simple_animated_icon.dart';
 
 // Entrypoint webpage of the application.
 class MainRoute extends StatefulWidget {
@@ -21,70 +21,14 @@ class _MainRouteState extends State<MainRoute> with TickerProviderStateMixin {
   final viewModel = ThemeViewModel();
   late double screenWidth;
 
-  late AnimationController
-      _animationController; // AnimationController for the theme icon
-  late Animation<double> _progress;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Calls the build method while the animation is in progress
-    _animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 2000))
-      ..addListener(() {
-        setState(() {});
-      });
-
-    _progress = Tween<double>(begin: 0, end: 1).animate(_animationController);
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     setState(() {
       screenWidth = MediaQuery.of(context).size.width;
     });
-    _progress = Tween<double>(begin: 0, end: 10).animate(_animationController);
 
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Salevy",
-              style: GoogleFonts.passionsConflict(fontSize: 90),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          toolbarHeight: 110,
-          actions: [
-            Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: IconButton(
-                  onPressed: () {
-                    if (viewModel.theme.value == ThemeMode.light) {
-                      viewModel.theme.value = ThemeMode.dark;
-                      _animationController.forward();
-                    } else {
-                      _animationController.reverse();
-                      viewModel.theme.value = ThemeMode.light;
-                    }
-                  },
-                  icon: SimpleAnimatedIcon(
-                    startIcon: Icons.sunny,
-                    endIcon: Icons.mode_night,
-                    progress: _progress,
-                  ),
-                ))
-          ],
-        ),
+        appBar: SalevyAppBar(title: "Salevy",),
         body: Center(
           child: SingleChildScrollView(
             child: Column(
@@ -114,8 +58,7 @@ class _MainRouteState extends State<MainRoute> with TickerProviderStateMixin {
                           utilities[index],
                           index: index,
                           reverse: true,
-                          visibilityKey:
-                               Key("congrats-easter-egg$index"),
+                          visibilityKey: Key("congrats-easter-egg$index"),
                         );
                       } else {
                         return UtilityCardText(
