44b287e7ecef2a20635364db94614ca3514c5fb4
Enjoyinbreaks <harshgoyenka@yahoo.com>
2022-10-11 20:57:10 +0530

    Added Descriptions

--- lib/data/shoe_datasource.dart
+++ lib/data/shoe_datasource.dart
@@ -6,56 +6,64 @@ const shoes = [
   Shoe(
       name: "Sporty Wind",
       price: 150,
-      description: "Made with the best quality leather from the United States. Fits easily and allows you to feel the wind while playing your favourite sport. With durable soals that never leave your side so that you can be worry-free.",
+      description:
+          "Made with the best quality leather from the United States. Fits easily and allows you to feel the wind while playing your favourite sport. With durable soals made with high-quality rubber, that never leaves your side.",
       origin: "USA",
       image: AssetImage("assets/images/sporty.png")),
   Shoe(
       name: "Ocean's Wave",
       price: 200,
-      description: "Wear it and surf like the waves of the ocean. Certainly, the right pair of shoes that can make you feel amazing and colors which refreshes your mood. ",
+      description:
+          "Wear it and surf like the waves of the ocean. Certainly, the right pair of shoes that can make you feel amazing with colors which refreshes your mood. With multiple lace buckels to keep it secure.",
       origin: "UK",
       image: AssetImage("assets/images/ocean.png")),
   Shoe(
       name: "Nightly Wear",
       price: 450,
-      description: "Make your shoes glow in the dark. This shoe is your best option if you are looking for a stylish, comfortable, and reliable pair of shoes. The shoe is made with the utmost attention to detail, providing the wearer with a more enjoyable and pleasurable experience.",
+      description:
+          "Make your shoes glow in the dark. This shoe is your best option if you are looking for a stylish, comfortable, and reliable pair of shoes. The shoe is made with the utmost attention to detail, providing the wearer with a more enjoyable and pleasurable experience.",
       origin: "Australia",
       image: AssetImage("assets/images/nightly.png")),
   Shoe(
       name: "Footballer's Companion",
       price: 500,
-      description: "The best soccer shoes you can ever get! These shoes have studs in the soal to prevent you from slipping during your match. The shoe body is hardened to secure you from any damage.",
+      description:
+          "The best soccer shoes you can ever get! These shoes have studs in the soal to prevent you from slipping during your match. With Aero-dynamic design to keep you steady. The shoe material is waterproof to keep your feet dry.",
       origin: "UK",
-      image:  AssetImage("assets/images/football.png")),
+      image: AssetImage("assets/images/football.png")),
   Shoe(
       name: "Mountaineer's Companion",
       price: 500,
-      description: "Give your legs some comfort while you scale the heights. Made for those who love adventure in their life, this shoe helps them to reach their summit easier. With added protection and waterproofing, to keep you going in all conditions. ",
+      description:
+          "Give your legs some comfort while you scale the heights. Made for those who love adventure in their life, this shoe helps them to reach their summit easier. With added protection and waterproofing, to keep you going in all conditions. ",
       origin: "Canada",
-      image:  AssetImage("assets/images/mountain.png")),
+      image: AssetImage("assets/images/mountain.png")),
   Shoe(
       name: "On The Go",
       price: 300,
-      description: "You can wear em' anytime, anyday! Thanks to the comfort they provide, these shoes improve the quality of life of their wearer.",
+      description:
+          "You can wear em' anytime, anyday! The shoe is made up of Polyvinyl Chloride that absorbs sweat, also offering Lightweight, Breathability and shock-absorption, bringing you a Dry and Cool life.",
       origin: "India",
-      image:  AssetImage("assets/images/onthego.png")),
+      image: AssetImage("assets/images/onthego.png")),
   Shoe(
       name: "Loafers",
       price: 350,
-      description: "Happy Feets! Happy Smiles!",
+      description:
+          "A casual slip-on! This shoe is made up of Faux leather that is perfect for party and with Ethinic wear. The footpad is made up of Jutis and Mojaris foam.",
       origin: 'Germany',
-      image:  AssetImage("assets/images/loafers.png")),
+      image: AssetImage("assets/images/loafers.png")),
   Shoe(
-      name: "Pumps",
+      name: "Sandals",
       price: 550,
-      description: "Look party ready al' the time",
+      description:
+          "Look party ready al' the time! Made up of Resin which allows to pull on these Sandals. The heel is 1.5 inches with a cushioned footbed, also has Textured outside.",
       origin: 'Italy',
-      image:  AssetImage('assets/images/pumps.png')),
+      image: AssetImage('assets/images/pumps.png')),
   Shoe(
       name: 'Sliders',
       price: 150,
       description:
-          "It's easire to put on slippers, than to cover whole world with carpet.",
+          "It's easire to put on slippers, than to cover whole world with carpet. This slider will benefit in Diabetes and Orthroped with it's medicated soft pads. The rubber soal is Anti-Skid.",
       origin: 'India',
       image: AssetImage('assets/images/sliders.png'))
 ];
