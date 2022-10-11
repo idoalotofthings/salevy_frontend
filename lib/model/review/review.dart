0a30d6e0cabcf9a0e8b1ac306b6dcf968f3eb476
Justin W <justinw@archlinux.localdomain>
2022-10-11 21:32:09 +0530

    Create review data class

---
+++ lib/model/review/review.dart
@@ -0,0 +1,11 @@
+import 'package:annotations/annotations.dart';
+
+part 'review.g.dart';
+
+@dataClass
+// ignore: unused_element
+class _AbsReview {
+  late String username;
+  late int rating;
+  late String message;
+}
