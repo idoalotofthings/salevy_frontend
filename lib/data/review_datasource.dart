5fe407b7c9620d859f2b7df572b64fbee89cfe03
Justin W <justinw@archlinux.localdomain>
2022-10-11 21:32:27 +0530

    Create review data source

---
+++ lib/data/review_datasource.dart
@@ -0,0 +1,11 @@
+// cc @enjoyinbreaks
+
+import 'package:salevy_frontend/model/review/review.dart';
+
+const reviews = [
+  Review(
+    username: "username", 
+    rating: 0, 
+    message: "message"
+  )
+];
