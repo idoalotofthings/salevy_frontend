975def0fe1f617ab2494bd122804651781be3f6f
Justin W <justinw@archlinux.localdomain>
2022-10-12 17:53:55 +0530

    Implement accessibility through snackbars

--- lib/ui/entrypoint_webpage.dart
+++ lib/ui/entrypoint_webpage.dart
@@ -5,9 +5,12 @@ import 'package:salevy_frontend/data/utility_datasource.dart';
 import 'package:salevy_frontend/ui/query_form_page.dart';
 import 'package:salevy_frontend/ui/viewmodel/viewmodel.dart';
 import 'package:salevy_frontend/ui/widgets/about_us_widget.dart';
+import 'package:salevy_frontend/ui/widgets/accessibility_snackbar.dart';
 import 'package:salevy_frontend/ui/widgets/app_bar.dart';
 import 'package:salevy_frontend/ui/widgets/product_showcase_widget.dart';
 import 'package:salevy_frontend/ui/widgets/utility_card_text.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+import '../dev/ext/buildcontext_ext.dart';
 
 // Entrypoint webpage of the application.
 class MainRoute extends StatefulWidget {
@@ -20,6 +23,7 @@ class MainRoute extends StatefulWidget {
 }
 
 class _MainRouteState extends State<MainRoute> with TickerProviderStateMixin {
+  static bool infoHasBeenShown = false;
   final viewModel = SalevyViewModel();
   late double screenWidth;
 
@@ -52,50 +56,73 @@ class _MainRouteState extends State<MainRoute> with TickerProviderStateMixin {
             openBuilder: (context, _) => const QueryFormRoute(),
           ),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                    padding: const EdgeInsets.only(bottom: 21.0),
-                    child: Container(
-                        height: 500,
-                        width: screenWidth,
-                        alignment: Alignment.topCenter,
-                        child: const ProductShowcaseWidget())),
-                Padding(
-                  padding: const EdgeInsets.all(21.0),
-                  child: Text(
-                    "Sneakers for Everyone!",
-                    style: GoogleFonts.passionsConflict(
-                        fontSize: 64, fontWeight: FontWeight.bold),
+        body: VisibilityDetector(
+          key: const Key("key@key"),
+          onVisibilityChanged: (_) {
+            if (!infoHasBeenShown) {
+              context.showSnackbar(accessibilitySnackbar(context,
+                  "Welcome to Salevy! Click on a shoe to view its details", () {
+                context.hideCurrentSnackbar();
+                context.showSnackbar(accessibilitySnackbar(context,
+                    "Click on the 'sun' icon at the top right of the window to toggle between themes",
+                    () {
+                  context.hideCurrentSnackbar();
+                  context.showSnackbar(accessibilitySnackbar(context,
+                      "Facing any issue? Click on the customer support icon just above this notification, to the right",
+                      () {
+                    context.hideCurrentSnackbar();
+                  }, buttonText: "Done"));
+                }));
+              }));
+
+              infoHasBeenShown = true;
+            }
+          },
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 21.0),
+                      child: Container(
+                          height: 500,
+                          width: screenWidth,
+                          alignment: Alignment.topCenter,
+                          child: const ProductShowcaseWidget())),
+                  Padding(
+                    padding: const EdgeInsets.all(21.0),
+                    child: Text(
+                      "Sneakers for Everyone!",
+                      style: GoogleFonts.passionsConflict(
+                          fontSize: 64, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: utilities.length,
-                    itemBuilder: ((context, index) {
-                      if (index == 1) {
-                        return UtilityCardText(
-                          utilities[index],
-                          index: index,
-                          reverse: true,
-                          visibilityKey: Key("congrats-easter-egg$index"),
-                        );
-                      } else {
-                        return UtilityCardText(
-                          utilities[index],
-                          index: index,
-                          visibilityKey: Key("congrats-easter-egg$index"),
-                        );
-                      }
-                    })),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: AboutUsWidget(),
-                )
-              ],
+                  ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: utilities.length,
+                      itemBuilder: ((context, index) {
+                        if (index == 1) {
+                          return UtilityCardText(
+                            utilities[index],
+                            index: index,
+                            reverse: true,
+                            visibilityKey: Key("congrats-easter-egg$index"),
+                          );
+                        } else {
+                          return UtilityCardText(
+                            utilities[index],
+                            index: index,
+                            visibilityKey: Key("congrats-easter-egg$index"),
+                          );
+                        }
+                      })),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: AboutUsWidget(),
+                  )
+                ],
+              ),
             ),
           ),
         ));
