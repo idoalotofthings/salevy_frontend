5014ac4ec99e7f65327027e96bf993ac5de620a8
Justin W <justinw@archlinux.localdomain>
2022-10-12 17:52:46 +0530

    Remove creators' names from about widget, replace with school code

--- lib/str/str_res.dart
+++ lib/str/str_res.dart
@@ -5,6 +5,6 @@ var aboutUs =
     "Salevy is a fictional brand. No part of this website has been copied from another source. This website is made for the Technophilia 'WebNotch' competition";
 
 var authors =
-    "This website has been created by Swastik Vats (@idoalotofthings) and Harsh Goyenka (@enjoyinbreaks)";
+    "This website has been created by students of school with the code TP 195";
 
 
